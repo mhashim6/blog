f535f4321828de07e62af47c71d51d25f75b4050
Muhammad Hashim <msg@mhashim6.me>
2022-05-01 05:42:47 +0200

    stop trying theming telegram

--- themes/hugo-theme-ava/assets/js/darkm.js
+++ themes/hugo-theme-ava/assets/js/darkm.js
@@ -1,11 +1,5 @@
 window.onload = function () {
   if (window.CSS && CSS.supports("color", "var(--sandman)")) {
-    const setTelegramCommentsTheme = function telegramTheme(dark = 0) {
-      if (window.document.getElementById("telegram_comments") != null) {
-        window.document.getElementById("telegram_comments").dataset.dark;
-      }
-    };
-
     const storage = localStorage.getItem("dark-mode");
 
     if (
@@ -13,7 +7,6 @@ window.onload = function () {
       window.matchMedia("(prefers-color-scheme: dark)").matches
     ) {
       document.documentElement.setAttribute("data-theme", "dark");
-      setTelegramCommentsTheme(1);
     }
 
     if (
@@ -21,29 +14,24 @@ window.onload = function () {
       window.matchMedia("(prefers-color-scheme: light)").matches
     ) {
       document.documentElement.setAttribute("data-theme", "light");
-      setTelegramCommentsTheme(0);
     }
 
     if (storage && storage === "dark") {
       document.documentElement.setAttribute("data-theme", "dark");
-      setTelegramCommentsTheme(1);
     }
 
     if (storage && storage === "light") {
       document.documentElement.setAttribute("data-theme", "light");
-      setTelegramCommentsTheme(0);
     }
 
     const toggleTheme = function toggleTheme(e) {
       if (e.currentTarget.classList.contains("dark--hidden")) {
         document.documentElement.setAttribute("data-theme", "dark");
         localStorage.setItem("dark-mode", "dark");
-        setTelegramCommentsTheme(1);
         return;
       }
       document.documentElement.setAttribute("data-theme", "light");
       localStorage.setItem("dark-mode", "light");
-      setTelegramCommentsTheme(0);
     };
     const toggleThemes = document.querySelectorAll(".theme__btn");
     toggleThemes.forEach(function (btn) {
