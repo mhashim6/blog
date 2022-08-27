ed145fe9cd8191eb46b5ccd45f335c18f043696b
Muhammad Hashim <msg@mhashim6.me>
2022-08-27 16:07:48 +0200

    reactive theme

--- themes/hugo-theme-ava/assets/js/darkm.js
+++ themes/hugo-theme-ava/assets/js/darkm.js
@@ -7,6 +7,9 @@ window.onload = function () {
       window.matchMedia("(prefers-color-scheme: dark)").matches
     ) {
       document.documentElement.setAttribute("data-theme", "dark");
+      document
+        .querySelector("meta[name='theme-color']")
+        .setAttribute("content", "#17191C");
     }
 
     if (
@@ -14,24 +17,39 @@ window.onload = function () {
       window.matchMedia("(prefers-color-scheme: light)").matches
     ) {
       document.documentElement.setAttribute("data-theme", "light");
+      document
+        .querySelector("meta[name='theme-color']")
+        .setAttribute("content", "#EDE3D9");
     }
 
     if (storage && storage === "dark") {
       document.documentElement.setAttribute("data-theme", "dark");
+      document
+        .querySelector("meta[name='theme-color']")
+        .setAttribute("content", "#17191C");
     }
 
     if (storage && storage === "light") {
       document.documentElement.setAttribute("data-theme", "light");
+      document
+        .querySelector("meta[name='theme-color']")
+        .setAttribute("content", "#EDE3D9");
     }
 
     const toggleTheme = function toggleTheme(e) {
       if (e.currentTarget.classList.contains("dark--hidden")) {
         document.documentElement.setAttribute("data-theme", "dark");
         localStorage.setItem("dark-mode", "dark");
+        document
+          .querySelector("meta[name='theme-color']")
+          .setAttribute("content", "#17191C");
         return;
       }
       document.documentElement.setAttribute("data-theme", "light");
       localStorage.setItem("dark-mode", "light");
+      document
+        .querySelector("meta[name='theme-color']")
+        .setAttribute("content", "#EDE3D9");
     };
     const toggleThemes = document.querySelectorAll(".theme__btn");
     toggleThemes.forEach(function (btn) {
